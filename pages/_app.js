c421ec2e476d8664e02bddf72c65be1a50c4e320
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-17 09:01:56 +0000

    hug

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import '../styles/globals.css';
 //components
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
+//transition
+import Transition from "../components/Transition";
+//router
+import { useRouter } from 'next/router';
+
+//framemotion
+import {AnimatePresence, motion } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return ( 
-    <Layout> <Component {...pageProps} /></Layout>
+    <Layout>
+      <AnimatePresence> <Component {...pageProps} /></AnimatePresence></Layout>
    
   )
 }
