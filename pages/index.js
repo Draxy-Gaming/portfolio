997ab9c17ee07d5e84c3b29ba259e394ac05dfa5
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-19 15:41:12 +0000

    blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 // components
-import ParticlesContainer from "../components/ParticlesContainer"
+import ParticlesContainer from "../components/ParticlesContainer";
 import Avatar from "../components/Avatar";
 import ProjectBtn from "../components/ProjectsBtn";
 
@@ -13,22 +13,36 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 const Home = () => {
-  return <div className="bg-primary/60 h-full">
-    
+  return (
+    <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/30 via-black/30 to-black/10">
         <div className="flex  text-center flex-col justify-center xl:pt-40 xl:text-left f-full container mx-auto">
           <h1 className="h1">
-            Transforming Ideas <br />Into  <span className="text-accent">Digital Reality</span>
+            Transforming Ideas <br />
+            Into <span className="text-accent">Digital Reality</span>
           </h1>
           {/*subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">Nisi enim nisi consectetur sit sint magna nostrud non excepteur ea incididunt proident aute velit. Velit aute pariatur incididunt exercitation .</p>
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            Nisi enim nisi consectetur sit sint magna nostrud non excepteur ea
+            incididunt proident aute velit. Velit aute pariatur incididunt
+            exercitation .
+          </p>
           <div className="flex justify-center  xl:hidden relative">
             <ProjectBtn />
           </div>
-           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            <ProjectBtn />
+          </motion.div>
+        </div>
       </div>
       <div>Image</div>
-  </div>;
+    </div>
+  );
 };
 
 export default Home;
