54546b5d2cf8275104b09f473722030a2f4512b2
Draxy-Gaming <140737843+Draxy-Gaming@users.noreply.github.com>
2023-12-19 16:09:05 +0000

    jh

--- pages/index.js
+++ pages/index.js
@@ -16,17 +16,26 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/30 via-black/30 to-black/10">
-        <div className="flex  text-center flex-col justify-center xl:pt-40 xl:text-left f-full container mx-auto">
-          <h1 className="h1">
+        <div className="flex  text-center flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <motion.h1
+           variants={fadeIn("down", 0.2)}
+           initial="hidden"
+           animate="show"
+           exit="hidden"
+          className="h1">
             Transforming Ideas <br />
             Into <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/*subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
-            Nisi enim nisi consectetur sit sint magna nostrud non excepteur ea
-            incididunt proident aute velit. Velit aute pariatur incididunt
-            exercitation .
-          </p>
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+           Bringing your ideas to life with innovative solutions. We specialize in creating digital experiences that captivate and inspire. Our team is dedicated to excellence, from concept to execution.
+          </motion.p>
           <div className="flex justify-center  xl:hidden relative">
             <ProjectBtn />
           </div>
@@ -35,12 +44,16 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
+            className="hidden xl:flex"
           >
             <ProjectBtn />
           </motion.div>
         </div>
       </div>
-      <div>Image</div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="xl:bg-right bg-none xl:bg-explosion xl:bg-cover xl:bg-no-repeat h-full w-full absolute mix-blend-color-dodge translate-z-0">
+        </div>
+      </div>
     </div>
   );
 };
