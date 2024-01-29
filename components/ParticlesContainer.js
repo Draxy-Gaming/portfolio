c794d6883dc338e0da6c83b798ce193847d73f09
MD IFTEKHAR FERDOUS <140737843+Draxy-Gaming@users.noreply.github.com>
2024-01-29 12:08:54 +0600

    update components/ParticlesContainer.js

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -2,9 +2,36 @@ import { Particles } from 'react-tsparticles';
 import { loadFull } from 'tsparticles'; 
 import React, { useCallback } from 'react';
 
-
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  //init
+  const particlesInit = useCallback( async ( engine) => {
+    await loadFull(engine);
+  }, []);
+  
+  const particlesLoaded = useCallback(async () => {}, []);
+  return(
+      <Particles id="tsparticles"  init={particlesInit} loaded={particlesLoaded} 
+      option={{
+        fullScreen: {enable: false},
+        background: {
+          color: {
+            value: '',
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onclick: {
+              enable: false,
+              mode: push,
+            },
+          },
+        },
+      }}
+      />
+    )
+
+
 };
 
 export default ParticlesContainer;
